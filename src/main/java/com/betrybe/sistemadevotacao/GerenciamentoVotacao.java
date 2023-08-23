d27ec3e59685bda9c0a52098069eceb8e286ff03
yurioneix <yurioneix18@gmail.com>
2023-08-23 15:13:45 -0300

    Corrige checkstyle

--- src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/betrybe/sistemadevotacao/GerenciamentoVotacao.java
@@ -2,7 +2,7 @@ package com.betrybe.sistemadevotacao;
 
 import java.util.ArrayList;
 
-public class GerenciamentoVotacao implements  GerenciamentoVotacaoInterface{
+public class GerenciamentoVotacao implements  GerenciamentoVotacaoInterface {
   ArrayList<String> pessoasCandidatas = new ArrayList<String>();
   ArrayList<String> pessoasEleitoras = new ArrayList<String>();
   ArrayList<String> cpfsComputados = new ArrayList<String>();
